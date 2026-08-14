b30f3c304953bdfa355b452425468974b109e552
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-08-14 19:24:41 +0530

    commit

--- gold_layer/business_analysis.sql
+++ gold_layer/business_analysis.sql
@@ -4,10 +4,11 @@ SELECT
     COUNT(DISTINCT product_key) AS total_products,
     SUM(sales_amount) AS total_revenue,
     SUM(quantity) AS total_quantity_sold,
-    ROUND(AVG(sales_amount), 2) AS avg_order_value
+    ROUND(AVG(sales_amount)::numeric, 2) AS avg_order_value
 FROM gold.fact_sales;
 
 
+--sales by country
 
 SELECT
     c.country,
@@ -20,19 +21,23 @@ GROUP BY c.country
 ORDER BY total_sales DESC;
 
 
+
+
+-- 3. Sales by Gender
+
+
 SELECT
     c.gender,
     COUNT(DISTINCT f.customer_key) AS total_customers,
     SUM(f.sales_amount) AS total_sales,
-    ROUND(AVG(f.sales_amount), 2) AS avg_sales
+    ROUND(AVG(f.sales_amount)::numeric, 2) AS avg_sales
 FROM gold.fact_sales f
 JOIN gold.dim_customers c ON f.customer_key = c.customer_key
 GROUP BY c.gender
 ORDER BY total_sales DESC;
 
 
-
-
+-- 4. Top 10 Products by Revenue 
 SELECT
     p.product_name,
     p.product_line,
@@ -47,17 +52,7 @@ LIMIT 10;
 
 
 
-
-SELECT
-    p.product_line,
-    p.category,
-    SUM(f.sales_amount) AS total_sales,
-    COUNT(DISTINCT f.order_number) AS total_orders
-FROM gold.fact_sales f
-JOIN gold.dim_products p ON f.product_key = p.product_key
-GROUP BY p.product_line, p.category
-ORDER BY total_sales DESC;
-
+-- 5. Sales by Product Line + Category
 
 
 SELECT
@@ -71,6 +66,8 @@ GROUP BY p.product_line, p.category
 ORDER BY total_sales DESC;
 
 
+
+-- 6. Top 10 Customers by Spending
 
 
 SELECT
@@ -84,3 +81,9 @@ JOIN gold.dim_customers c ON f.customer_key = c.customer_key
 GROUP BY c.full_name, c.country, c.gender
 ORDER BY total_spent DESC
 LIMIT 10;
+
+
+
+
+
+

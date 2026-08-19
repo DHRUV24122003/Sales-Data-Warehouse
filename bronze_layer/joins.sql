acedfed1d6f9913f8d18a920edb70951d5f9e03b
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-08-19 10:45:43 +0530

    commit

--- bronze_layer/joins.sql
+++ bronze_layer/joins.sql
@@ -81,3 +81,103 @@
     ORDER BY total_sales DESC;
 
 
+
+
+
+--Har customer ka naam, country aur unki total sales dikhao.
+--join info , sales details and location
+select * from bronze.crm_cust_info
+select * from bronze.crm_sales_details
+select * from bronze.erp_loc_a101
+
+SELECT
+    c.cst_firstname,
+    c.cst_lastname,
+    l."CNTRY" AS country,
+    SUM(s.sls_sales) AS total_sales
+FROM bronze.crm_sales_details s
+INNER JOIN bronze.crm_cust_info c
+    ON s.sls_cust_id = c.cst_id
+INNER JOIN bronze.erp_loc_a101 l
+    ON REPLACE(l."CID", '-', '') = c.cst_key
+GROUP BY c.cst_firstname, c.cst_lastname, l."CNTRY"
+ORDER BY total_sales DESC;
+
+
+
+-- Har product ka naam, category, product line aur uski total sales + total quantity dikhao.
+-- Sales ke hisaab se descending order mein.
+
+
+select * from bronze.crm_prd_info
+select * from bronze.crm_sales_details
+select * from bronze.erp_px_cat_g1v2
+
+SELECT DISTINCT "ID"
+FROM bronze.erp_px_cat_g1v2
+LIMIT 20;
+
+SELECT DISTINCT RIGHT(prd_key, 10) AS prd_key_last10
+FROM bronze.crm_prd_info
+
+SELECT
+    p.prd_nm AS product_name,
+    pc."CAT" AS category,
+    p.prd_line AS product_line,
+    SUM(s.sls_sales) AS total_sales,
+    SUM(s.sls_quantity) AS total_quantity
+FROM bronze.crm_sales_details s
+INNER JOIN bronze.crm_prd_info p
+    ON s.sls_prd_key = RIGHT(p.prd_key, 10)
+LEFT JOIN bronze.erp_px_cat_g1v2 pc
+    ON REPLACE(LEFT(p.prd_key, 5), '-', '_') = pc."ID"
+GROUP BY p.prd_nm, pc."CAT", p.prd_line
+ORDER BY total_sales DESC;
+
+
+--the customers who did not give any order
+--their name gender and country
+select * from bronze.crm_prd_info
+select * from bronze.crm_sales_details
+select * from bronze.erp_px_cat_g1v2
+select * from bronze.crm_cust_info
+select * from bronze.erp_loc_a101
+
+
+
+SELECT
+    c.cst_firstname,
+    c.cst_lastname,
+    c.cst_gndr AS gender,
+    l."CNTRY" AS country
+FROM bronze.crm_cust_info c
+LEFT JOIN bronze.crm_sales_details s
+    ON c.cst_id = s.sls_cust_id
+LEFT JOIN bronze.erp_loc_a101 l
+    ON REPLACE(l."CID", '-', '') = c.cst_key
+WHERE s.sls_cust_id IS NULL
+ORDER BY c.cst_firstname;
+
+
+-- Har country ka total sales, total orders aur total unique customers dikhao.
+-- Highest sales wale country ko pehle dikhao.
+
+select * from bronze.crm_prd_info
+select * from bronze.crm_sales_details
+select * from bronze.erp_px_cat_g1v2
+select * from bronze.crm_cust_info
+select * from bronze.erp_loc_a101
+
+
+select l."CNTRY" as country,
+       sum(s.sls_sales) as total_sales,
+       count(s.sls_ord_num) as order_count,
+       count(distinct(cst_id)) as unique_cst
+
+    FROM bronze.crm_cust_info c
+LEFT JOIN bronze.crm_sales_details s
+    ON c.cst_id = s.sls_cust_id
+LEFT JOIN bronze.erp_loc_a101 l
+    ON REPLACE(l."CID", '-', '') = c.cst_key
+    GROUP BY l."CNTRY" 
+
